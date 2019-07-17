ed145a532cff2fb24cc81a29a8a4e87c75f55a62
zenywallet <zenywallet@bitzeny.jp>
2019-07-17 12:03:23 +0900

    libbtc lib, test

--- libbtc.nim
+++ libbtc.nim
@@ -1,9 +1,273 @@
 # Copyright (c) 2019 zenywallet
 
-import os
+import os, byteutils, marshal, sequtils
 
 const libbtcPath = splitPath(currentSourcePath()).head & "/deps/libbtc"
 {.passL: libbtcPath & "/.libs/libbtc.a".}
 {.passL: libbtcPath & "/src/secp256k1/.libs/libsecp256k1.a".}
 
 proc btc_random_bytes*(buf: var openarray[byte], len: uint32, update_seed: uint8): uint8 {.importc.}
+
+type
+  uint8_t* = uint8
+  uint32_t* = uint32
+  btc_bool* = bool
+
+const
+  BTC_ECKEY_UNCOMPRESSED_LENGTH* = 65
+  BTC_ECKEY_COMPRESSED_LENGTH* = 33
+  BTC_ECKEY_PKEY_LENGTH* = 32
+  #BTC_ECKEY_PKEY_LENGTH* = 32
+  BTC_HASH_LENGTH* = 32
+
+template BTC_MIN*(a, b: untyped): untyped =
+  (if ((a) < (b)): (a) else: (b))
+
+template BTC_MAX*(a, b: untyped): untyped =
+  (if ((a) > (b)): (a) else: (b))
+
+type
+  uint256* = array[32, uint8_t]
+  uint160* = array[20, uint8_t]
+
+type
+  btc_dns_seed* {.bycopy.} = object
+    domain*: array[256, char]
+
+  btc_chainparams* {.bycopy.} = object
+    chainname*: array[32, char]
+    b58prefix_pubkey_address*: uint8_t
+    b58prefix_script_address*: uint8_t
+    bech32_hrp*: array[5, char]
+    b58prefix_secret_address*: uint8_t ## !private key
+    b58prefix_bip32_privkey*: uint32_t
+    b58prefix_bip32_pubkey*: uint32_t
+    netmagic*: array[4, cuchar]
+    genesisblockhash*: uint256
+    default_port*: cint
+    dnsseeds*: array[8, btc_dns_seed]
+
+  btc_checkpoint* {.bycopy.} = object
+    height*: uint32_t
+    hash*: cstring
+    timestamp*: uint32_t
+    target*: uint32_t
+
+
+  btc_chainparams2* {.bycopy.} = object
+    chainname*: array[32, char]
+
+#var btc_chainparams_main*: btc_chainparams
+
+#var btc_chainparams_test*: btc_chainparams
+
+#var btc_chainparams_regtest*: btc_chainparams
+
+##  the mainnet checkpoins, needs a fix size
+
+#var btc_mainnet_checkpoint_array*: array[21, btc_checkpoint]
+
+#type
+#  btc_chainparams* {.importc: "btc_chainparams", bycopy.} = object
+
+var btc_chainparams_main* {.importc.}: btc_chainparams
+
+var btc_chainparams_test* {.importc.}: btc_chainparams
+
+var btc_chainparams_regtest* {.importc.}: btc_chainparams
+
+var btc_mainnet_checkpoint_array* {.importc.}: ptr btc_checkpoint
+
+
+#echo btc_chainparams_main.default_port
+
+proc hd_gen_master*(chain: ptr btc_chainparams; masterkeyhex: cstring; strsize: csize): btc_bool {.importc.}
+
+proc hd_derive*(chain: ptr btc_chainparams; masterkey: cstring; keypath: cstring;
+               extkeyout: cstring; extkeyout_size: csize): btc_bool {.importc.}
+
+#proc hd_print_node*(chain: ptr btc_chainparams; nodeser: cstring): btc_bool {.importc.}
+
+proc btc_ecc_start*() {.importc.}
+
+proc btc_ecc_stop*() {.importc.}
+
+const
+  BTC_BIP32_CHAINCODE_SIZE* = 32
+
+type
+  btc_hdnode* {.bycopy.} = object
+    depth*: uint32_t
+    fingerprint*: uint32_t
+    child_num*: uint32_t
+    chain_code*: array[BTC_BIP32_CHAINCODE_SIZE, uint8_t]
+    private_key*: array[BTC_ECKEY_PKEY_LENGTH, uint8_t]
+    public_key*: array[BTC_ECKEY_COMPRESSED_LENGTH, uint8_t]
+
+proc btc_hdnode_deserialize*(str: cstring; chain: ptr btc_chainparams;
+                            node: ptr btc_hdnode): btc_bool {.importc.}
+
+proc btc_hdnode_get_p2pkh_address*(node: ptr btc_hdnode; chain: ptr btc_chainparams;
+                                  str: cstring; strsize: cint) {.importc.}
+
+proc btc_hdnode_has_privkey*(node: ptr btc_hdnode): btc_bool {.importc.}
+
+proc btc_hdnode_get_pub_hex*(node: ptr btc_hdnode; str: cstring; strsize: ptr csize): btc_bool {.importc.}
+
+proc btc_hdnode_serialize_public*(node: ptr btc_hdnode; chain: ptr btc_chainparams;
+                                 str: cstring; strsize: cint) {.importc.}
+
+proc btc_hdnode_serialize_private*(node: ptr btc_hdnode; chain: ptr btc_chainparams;
+                                  str: cstring; strsize: cint) {.importc.}
+
+proc btc_base58_encode_check*(data: ptr UncheckedArray[uint8_t]; datalen: cint; str: cstring;
+                             strsize: cint): cint {.importc.}
+
+#proc printf(formatstr: cstring) {.header: "<stdio.h>", importc: "printf", varargs.}
+#proc strncpy(s1: ptr UncheckedArray[char]; s2: cstring; n: csize): cstring {.importc.}
+
+proc hd_print_node*(chain: ptr btc_chainparams; nodeser: cstring): btc_bool =
+  var node: btc_hdnode
+  if not btc_hdnode_deserialize(nodeser, chain, addr(node)):
+    return false
+  var strsize: csize = 128
+  var str: cstring = newString(strsize) #array[strsize, char]
+  btc_hdnode_get_p2pkh_address(addr(node), chain, str, cast[cint](strsize))
+  echo "ext key: ", nodeser
+  const privkey_wif_size_bin: csize = 34
+  var pkeybase58c: array[privkey_wif_size_bin, uint8_t]
+  pkeybase58c[0] = chain.b58prefix_secret_address
+  pkeybase58c[33] = 1
+  ##  always use compressed keys
+  const privkey_wif_size: csize = 128
+  var privkey_wif: cstring = newString(privkey_wif_size) #array[privkey_wif_size, char]
+  #memcpy(addr(pkeybase58c[1]), node.private_key, BTC_ECKEY_PKEY_LENGTH)
+  echo $$node.private_key
+  echo node.private_key.toHex()
+  copyMem(addr pkeybase58c[1], unsafeAddr node.private_key, BTC_ECKEY_PKEY_LENGTH)
+  assert(btc_base58_encode_check(cast[ptr UncheckedArray[uint8_t]](addr pkeybase58c),
+                                 cast[cint](privkey_wif_size_bin), privkey_wif,
+                                 cast[cint](privkey_wif_size)) != 0)
+
+  if btc_hdnode_has_privkey(addr(node)):
+    echo "privatekey WIF: ", $privkey_wif
+  echo "depth: ", $node.depth
+  echo "child index: ", $node.child_num
+  echo "p2pkh address: ", $str
+  #echo "p2wpkh address: " & $str
+  if not btc_hdnode_get_pub_hex(addr(node), str, addr(strsize)):
+    return false
+  echo "pubkey hex: ", str
+  strsize = 128
+  btc_hdnode_serialize_public(addr(node), chain, str, cast[cint](strsize))
+  echo "extended pubkey: ", str
+  return true
+
+proc btc_hd_generate_key*(node: ptr btc_hdnode; keypath: cstring;
+                         keymaster: ptr uint8_t; chaincode: ptr uint8_t;
+                         usepubckd: btc_bool): btc_bool {.importc.}
+type
+  chainparams* = object
+    chainname*: string
+    b58prefix_pubkey_address*: uint8
+    b58prefix_script_address*: uint8
+    bech32_hrp*: string
+    b58prefix_secret_address*: uint8
+    b58prefix_bip32_privkey*: uint32
+    b58prefix_bip32_pubkey*: uint32
+    netmagic*: array[4, byte]
+    genesisblockhash*: array[32, byte]
+    default_port*: int32
+    dnsseeds*: seq[string]
+
+
+proc set_chainparams*(params: chainparams): btc_chainparams =
+  var chain: btc_chainparams = btc_chainparams(
+    b58prefix_pubkey_address: params.b58prefix_pubkey_address,
+    b58prefix_script_address: params.b58prefix_script_address,
+    b58prefix_secret_address: params.b58prefix_secret_address,
+    b58prefix_bip32_privkey: params.b58prefix_bip32_privkey,
+    b58prefix_bip32_pubkey: params.b58prefix_bip32_pubkey,
+    default_port: params.default_port)
+
+  zeroMem(addr chain.chainname, chain.chainname.len)
+  for i, c in params.chainname:
+    if i > chain.chainname.high:
+      echo "ERROR[set_chainparams]: chainname too long"
+      break
+    chain.chainname[i] = c
+
+  zeroMem(addr chain.bech32_hrp, chain.bech32_hrp.len)
+  for i, c in params.bech32_hrp:
+    if i > chain.bech32_hrp.high:
+      echo "ERROR[set_chainparams]: bech32_hrp too long"
+      break
+    chain.bech32_hrp[i] = c
+
+  zeroMem(addr chain.netmagic, chain.netmagic.len)
+  for i, c in params.netmagic:
+    if i > chain.netmagic.high:
+      echo "ERROR[set_chainparams]: netmagic too long"
+      break
+    chain.netmagic[i] = cast[cuchar](c)
+
+  zeroMem(addr chain.genesisblockhash, chain.genesisblockhash.len)
+  for i, c in params.genesisblockhash:
+    if i > chain.genesisblockhash.high:
+      echo "ERROR[set_chainparams]: genesisblockhash too long"
+      break
+    chain.genesisblockhash[i] = c
+
+  zeroMem(addr chain.dnsseeds, btc_dns_seed.domain.len * chain.dnsseeds.len)
+  for i, seed in params.dnsseeds:
+    if i > chain.dnsseeds.high:
+      echo "ERROR[set_chainparams]: dnsseeds too long"
+      break
+    for j, c in seed:
+      if j > chain.dnsseeds[i].domain.high:
+        echo "ERROR[set_chainparams]: domain too long"
+        break
+      chain.dnsseeds[i].domain[j] = c
+  chain
+
+proc btc_ecc_sign_compact_recoverable*(private_key: ptr cuchar; hash: uint256;
+                                      sigrec: ptr cuchar; outlen: ptr csize;
+                                      #sigrec: ptr cuchar;
+                                      recid: ptr cint): btc_bool {.importc.}
+
+const
+  SHA256_DIGEST_LENGTH = 32
+
+#type
+#  sha2_byte = uint8_t
+
+proc sha256_Raw*(data: ptr cuchar; len: csize;
+                digest: array[SHA256_DIGEST_LENGTH, uint8_t]) {.importc.}
+
+proc btc_hash*(datain: ptr cuchar; length: csize; hashout: uint256) =
+  sha256_Raw(datain, length, hashout)
+  sha256_Raw(cast[ptr cuchar](unsafeAddr hashout), SHA256_DIGEST_LENGTH, hashout)
+
+func var_uint*(n: uint64): seq =
+  if n < 0xfd: @[uint8(n)]
+  elif n <= 0xffff: concat(@[uint8(0xfd)], toSeq(cast[array[2, byte]](uint16(n))))
+  elif n <= 0xffffffff'u64: concat(@[uint8(0xfe)], toSeq(cast[array[4, byte]](uint32(n))))
+  else: concat(@[uint8(0xff)], toSeq(cast[array[8, byte]](uint64(n))))
+
+var bitzeny_chainparams = chainparams(
+  chainname: "main",
+  b58prefix_pubkey_address: 0x51,
+  b58prefix_script_address: 0x05,
+  bech32_hrp: "sz",
+  b58prefix_secret_address: 0x80,
+  b58prefix_bip32_privkey: 0x0488ade4,
+  b58prefix_bip32_pubkey: 0x0488b21e,
+  netmagic: [byte 0xda, 0xad, 0xbe, 0xf9],
+  genesisblockhash: [byte 0x5c, 0x22, 0xde, 0x9b, 0x02, 0xe7, 0x15, 0x02,
+                    0xfd, 0x7e, 0xdd, 0xa7, 0xb6, 0x1a, 0x54, 0x3c,
+                    0xd2, 0x3f, 0xb6, 0x2e, 0xa2, 0x37, 0x7f, 0xdb,
+                    0xbd, 0x95, 0xf5, 0x79, 0x0c, 0x3a, 0x00, 0x00],
+  default_port: 9253,
+  dnsseeds: @["seed.bitzeny.jp"])
+
+var bitzeny_chain*: btc_chainparams = set_chainparams(bitzeny_chainparams)
--- tests/test_libbtc.nim
+++ tests/test_libbtc.nim
@@ -1,6 +1,6 @@
 # Copyright (c) 2019 zenywallet
 
-import unittest, byteutils, marshal, sequtils
+import unittest, byteutils, marshal, sequtils, base64
 import ../libbtc
 
 test "btc_random_bytes array":
@@ -14,3 +14,134 @@ test "btc_random_bytes seq":
   var ret = btc_random_bytes(buf, 32, 0)
   echo "ret=", ret, " ", buf.toHex
   check(ret == 1)
+
+test "bip44-1":
+  #[
+  mnemonic = "achieve evoke pigeon twin lake build sign inform nice sick glance end tube shadow floor"
+  seed = "80a20b8ad8e285266196504d0e4a66d2397ccce83d024237aaf7496e3cb4b0ad26fec4bc35b98c7a03c1e707e29c31185d078113a1004d5880c2edcdb788dfcc"
+  rootkey = "xprv9s21ZrQH143K4KinPjY2wtbeAW62uSqNbc4UPGjt3cpyVc5Udzc5odqwZbN14qLCFkLdAezMrN2BvsYNjqS48GfKdijwfdXg9Jo8n8fQoYf"
+
+  44 0 0 0 xprv xprv9z3yZx89zwd442ix6DaHvcStmSYYV5xKdVG9vNS6RMkytGYZ5myQeF1aagTzkk2w7e1ejTCVCeNT6sj2HYzAAgvuqBYZ53T25PgRMEFtmMx
+  44 0 0 0 xpub xpub6D3KyTf3qKBMGWoRCF7JHkPdKUP2tYgAziBkikqhyhHxm4shdKHfC3L4RxdeNRDeFGpZGHEoEGoCorRcodbjKf7E6zAg7XkKUHfnrGNDz8q
+  m/44'/0'/0'/0 extended xprvA2LGQZVkGpJaFnEiawYHPJc3Yy1c1DboZkF9Aw9oNP66jKfXyNZc2BhM5SCefvXKMhyZheQdfXRYw1PgRTfKVXvnfzRgtDTUBBRRbAX4eGN
+  m/44'/0'/0'/0 extended xpub6FKcp52e7BrsUGKBgy5HkSYn6zr6QgKevyAjyKZQvid5c7zgWusrZz1pvhRNDB6WJqR6RzBc5btMVHD3W8Xb39ut8tKgAGrQF6SehBzw2Y1
+  18HVNi1YEtnyLJJVkPzcyyssryZjREw6Sq  027bc1c0bd02704f2c36ce1bd5b8bc1990d798bb3b0aa008bb2c3b1237110f0155  KyeZweoUyz7AwEjfK7Cq54W6ocS3HqWFcU4DtmhhuTuTWPnP8zDE
+
+  m/44'/0'/0'/0/0   18HVNi1YEtnyLJJVkPzcyyssryZjREw6Sq  027bc1c0bd02704f2c36ce1bd5b8bc1990d798bb3b0aa008bb2c3b1237110f0155  KyeZweoUyz7AwEjfK7Cq54W6ocS3HqWFcU4DtmhhuTuTWPnP8zDE
+  m/44'/0'/0'/0/1   1AK3na6js6SwaxBN76XXpuTgtLchzpPSaS  02998cf0baac2693ba44e4acb03fc1212117d2287c139e638e5fe3edcb96412114  KyyxoE8V9AoSAUhqigSBkAkbvDSzAneKtRSUxHUFq5Arsi4w5soR
+  m/44'/0'/0'/0/2   16GxWG4bMaCFRWq179vh2wQG1yGGQNwNz5  02ee3a492356c0374664e49575242ca1435bd864fe283fee37a19efeee24a5c1fb  L2okuHzASmeFxwPM6Td2iXxsEGu3ACiAfSFh5bfRHyTLcBHPwhoM
+  m/44'/0'/0'/0/3   1JzpmtEzKj1BPVeMGdpHt5y55djbZ5mX98  022c646594acf9d03e9e861a3ac2ae65e4797525fa33b9434a6a4b1b90d148186a  L54y4TC2faBxp7uSyXrsvY5SU2zf1sSJEX1bzUkosKFdpD3CtToS
+  m/44'/0'/0'/0/4   1H9XLEgUVe67mPwiZQeEw8UBgMD8v3opqh  02e59916ae8f489430f88666b39c3dd911c86a53b2de0b81d3e31064a754687a31  L46wiX9EVk6AP4WRqJviyznfHS69KZ9aoB3PPh9cgiKtPhq6ExYY
+  m/44'/0'/0'/0/5   1KLkmyVTSduAxM6v2gSsH24dzYek7BnsEr  024b8778209fb3b04a5157b5840298dc12703045f485ea432eef05c1e8112789ca  L3Di5r7xPCsw2cWjyiXnCqVh2xi9aPKinWUNRV4b8FzQTjJSJLVc
+
+  m/0/0   18HVNi1YEtnyLJJVkPzcyyssryZjREw6Sq  027bc1c0bd02704f2c36ce1bd5b8bc1990d798bb3b0aa008bb2c3b1237110f0155  NA
+  m/0/1   1AK3na6js6SwaxBN76XXpuTgtLchzpPSaS  02998cf0baac2693ba44e4acb03fc1212117d2287c139e638e5fe3edcb96412114  NA
+  m/0/2   16GxWG4bMaCFRWq179vh2wQG1yGGQNwNz5  02ee3a492356c0374664e49575242ca1435bd864fe283fee37a19efeee24a5c1fb  NA
+  m/0/3   1JzpmtEzKj1BPVeMGdpHt5y55djbZ5mX98  022c646594acf9d03e9e861a3ac2ae65e4797525fa33b9434a6a4b1b90d148186a  NA
+  m/0/4   1H9XLEgUVe67mPwiZQeEw8UBgMD8v3opqh  02e59916ae8f489430f88666b39c3dd911c86a53b2de0b81d3e31064a754687a31  NA
+  ]#
+
+  #let privkey: cstring = "xpub6D3KyTf3qKBMGWoRCF7JHkPdKUP2tYgAziBkikqhyhHxm4shdKHfC3L4RxdeNRDeFGpZGHEoEGoCorRcodbjKf7E6zAg7XkKUHfnrGNDz8q"
+  #let privkey: cstring = "xprv9s21ZrQH143K2JF8RafpqtKiTbsbaxEeUaMnNHsm5o6wCW3z8ySyH4UxFVSfZ8n7ESu7fgir8imbZKLYVBxFPND1pniTZ81vKfd45EHKX73"
+  #var masterkey: cstring = "xprv9s21ZrQH143K3C5hLMq2Upsh8mf9Z1p5C4QuXJkiodSSihp324YnWpFfRjvP7gqocJKz4oakVwZn5cUgRYTHtNRvGqU5DU2Gn8MPM9jHvfC" #cast[cstring](seed)
+
+  btc_ecc_start()
+  let privkey: cstring = "xprv9s21ZrQH143K4KinPjY2wtbeAW62uSqNbc4UPGjt3cpyVc5Udzc5odqwZbN14qLCFkLdAezMrN2BvsYNjqS48GfKdijwfdXg9Jo8n8fQoYf"
+  let keypath: cstring = "m/44'/0'/0'"
+  let extkeyout_size: csize = 128
+  let extkeyout: cstring = newString(extkeyout_size)
+  var ret: bool = hd_derive(addr bitzeny_chain, privkey, keypath, extkeyout, extkeyout_size)
+  echo "ret=", ret, " ", extkeyout
+  check(ret == true and extkeyout == "xprv9z3yZx89zwd442ix6DaHvcStmSYYV5xKdVG9vNS6RMkytGYZ5myQeF1aagTzkk2w7e1ejTCVCeNT6sj2HYzAAgvuqBYZ53T25PgRMEFtmMx")
+
+  var node: btc_hdnode
+  ret = btc_hdnode_deserialize(extkeyout, addr bitzeny_chain, addr node)
+  var strsize: csize = 128
+  var str: cstring = newString(strsize)
+  ret = btc_hdnode_get_pub_hex(addr node, str, addr strsize)
+  echo "pub hex: ", str, " ", strsize
+  strsize = 128
+  btc_hdnode_serialize_public(addr node, addr bitzeny_chain, str, cast[cint](strsize))
+  echo "xpub: ", str, " ", strsize
+  check(str == "xpub6D3KyTf3qKBMGWoRCF7JHkPdKUP2tYgAziBkikqhyhHxm4shdKHfC3L4RxdeNRDeFGpZGHEoEGoCorRcodbjKf7E6zAg7XkKUHfnrGNDz8q")
+  btc_ecc_stop()
+
+test "bip44-2":
+  btc_ecc_start()
+  let privkey: cstring = "xprv9s21ZrQH143K4KinPjY2wtbeAW62uSqNbc4UPGjt3cpyVc5Udzc5odqwZbN14qLCFkLdAezMrN2BvsYNjqS48GfKdijwfdXg9Jo8n8fQoYf"
+  let keypath: cstring = "m/44'/0'/0'/0"
+  let extkeyout_size: csize = 128
+  let extkeyout: cstring = newString(extkeyout_size)
+  var ret: bool = hd_derive(addr bitzeny_chain, privkey, keypath, extkeyout, extkeyout_size)
+  echo "ret=", ret, " ", extkeyout
+  check(ret == true and extkeyout == "xprvA2LGQZVkGpJaFnEiawYHPJc3Yy1c1DboZkF9Aw9oNP66jKfXyNZc2BhM5SCefvXKMhyZheQdfXRYw1PgRTfKVXvnfzRgtDTUBBRRbAX4eGN")
+
+  var node: btc_hdnode
+  ret = btc_hdnode_deserialize(extkeyout, addr bitzeny_chain, addr node)
+  var strsize: csize = 128
+  var str: cstring = newString(strsize)
+  ret = btc_hdnode_get_pub_hex(addr node, str, addr strsize)
+  echo "pub hex: ", str, " ", strsize
+  strsize = 128
+  btc_hdnode_serialize_public(addr node, addr bitzeny_chain, str, cast[cint](strsize))
+  echo "xpub: ", str, " ", strsize
+  check(str == "xpub6FKcp52e7BrsUGKBgy5HkSYn6zr6QgKevyAjyKZQvid5c7zgWusrZz1pvhRNDB6WJqR6RzBc5btMVHD3W8Xb39ut8tKgAGrQF6SehBzw2Y1")
+  btc_ecc_stop()
+
+test "bip44-3":
+  btc_ecc_start()
+  let privkey: cstring = "xpub6D3KyTf3qKBMGWoRCF7JHkPdKUP2tYgAziBkikqhyhHxm4shdKHfC3L4RxdeNRDeFGpZGHEoEGoCorRcodbjKf7E6zAg7XkKUHfnrGNDz8q"
+  let keypath: cstring = "m/0/0"
+  let extkeyout_size: csize = 128
+  let extkeyout: cstring = newString(extkeyout_size)
+  var ret: bool = hd_derive(addr bitzeny_chain, privkey, keypath, extkeyout, extkeyout_size)
+  echo "ret=", ret, " ", extkeyout
+
+  var node: btc_hdnode
+  ret = btc_hdnode_deserialize(extkeyout, addr bitzeny_chain, addr node)
+  var strsize: csize = 128
+  var str: cstring = newString(strsize)
+  ret = btc_hdnode_get_pub_hex(addr node, str, addr strsize)
+  echo "pub hex: ", str, " ", strsize
+  check(str == "027bc1c0bd02704f2c36ce1bd5b8bc1990d798bb3b0aa008bb2c3b1237110f0155")
+  strsize = 128
+  btc_hdnode_serialize_public(addr node, addr bitzeny_chain, str, cast[cint](strsize))
+  echo "xpub: ", str, " ", strsize
+  discard hd_print_node(addr bitzeny_chain, str)
+  btc_ecc_stop()
+
+test "var_uint":
+  let v: uint64 = 5000000000000000'u64
+  echo var_uint(v)
+
+test "sign message":
+  btc_ecc_start()
+  var message = "This is an example of a signed message."
+  var messagePrefix = "\u0018BitZeny Signed Message:\n"
+  echo message.len
+  echo messagePrefix.len
+  var buf: seq[cuchar] = concat(toSeq(messagePrefix), cast[seq[cuchar]](var_uint(uint64(message.len))), toSeq(message))
+  echo buf
+  echo "buf len=" & $buf.len
+  check(buf.len == 65)
+  echo cast[seq[byte]](buf).toHex()
+  var hashout: uint256
+  btc_hash(cast[ptr cuchar](addr buf[0]), buf.len, hashout)
+  echo "hashout hex=", hashout.toHex()
+  var priv = @[byte 15, 201, 181, 158, 189, 105, 123, 206, 195, 117, 163, 4, 92, 59, 51, 84, 252, 111, 255, 47, 128, 169, 116, 196, 52, 6, 89, 49, 254, 163, 157, 173]
+  var sigrec = newSeq[cuchar](buf.len)
+  var outlen: csize = -1
+  var fCompressed = true
+  var recid: cint
+  var ret3 = btc_ecc_sign_compact_recoverable(cast[ptr cuchar](addr priv[0]), hashout, cast[ptr cuchar](addr sigrec[1]), addr outlen, addr recid)
+  if fCompressed:
+    sigrec[0] = (27 + recid + 4).cuchar
+  else:
+    sigrec[0] = (27 + recid).cuchar
+  let sign_hex = cast[seq[byte]](sigrec).toHex()
+  let sign_encode = sigrec.encode()
+  echo sign_hex
+  echo sign_encode
+  check(sign_hex == "2094095a32cf86a51e20740d80c4c700e86e7b37105a4a2850ec35c9ca7ac7b16768c259aa4f72a4958c34c7050ac7e349e090d276e363e2316487f4171115a977")
+  check(sign_encode == "IJQJWjLPhqUeIHQNgMTHAOhuezcQWkooUOw1ycp6x7FnaMJZqk9ypJWMNMcFCsfjSeCQ0nbjY+IxZIf0FxEVqXc=")
+  btc_ecc_stop()
