c615a87c52f98a33b30aee68ffb93f8f467fb6df
zenywallet <zenywallet@bitzeny.jp>
2019-08-03 01:35:27 +0900

    cipher nim to js wasm

---
+++ src/cipher.nim
@@ -0,0 +1,15 @@
+# Copyright (c) 2019 zenywallet
+# nim c -d:release -d:emscripten -o:cipher.js cipher.nim
+
+import ctrmode, byteutils, algorithm
+
+var ctr: CTR
+
+proc cipher_init*(key, enc_iv, dec_iv: array[32, byte]) {.exportc: "cipher_init".} =
+  ctr.init(key, enc_iv, dec_iv)
+
+proc cipher_enc*(in_blk, out_blk: ptr UncheckedArray[byte]) {.exportc: "cipher_enc".} =
+  ctr.encrypt(in_blk, out_blk)
+
+proc cipher_dec*(in_blk, out_blk: ptr UncheckedArray[byte]) {.exportc: "cipher_dec".} =
+  ctr.decrypt(in_blk, out_blk)
