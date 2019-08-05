6e6606c5a0d06f1d6540da77821cf44c5cc42b41
zenywallet <zenywallet@bitzeny.jp>
2019-08-05 20:26:18 +0900

    does not work with windows

--- src/events.nim
+++ src/events.nim
@@ -6,39 +6,27 @@ type
   Event* = object
     cond: Cond
     lock: Lock
-    when not defined(Windows):
-      signal: bool
+    signal: bool
 
 proc createEvent*(): Event =
   var ev: Event
   initCond(ev.cond)
   initLock(ev.lock)
-  when not defined(Windows):
-    ev.signal = false
+  ev.signal = false
 
 proc closeEvent*(ev: var Event) {.inline.} =
   deinitLock(ev.lock)
   deinitCond(ev.cond)
 
-when defined(Windows):
-  proc waitFor*(ev: var Event) =
-    acquire(ev.lock)
+proc waitFor*(ev: var Event) =
+  acquire(ev.lock)
+  if not ev.signal:
     wait(ev.cond, ev.lock)
-    release(ev.lock)
+  ev.signal = false
+  release(ev.lock)
 
-  proc setEvent*(ev: var Event) =
-    signal(ev.cond)
-
-else:
-  proc waitFor*(ev: var Event) =
-    acquire(ev.lock)
-    if not ev.signal:
-      wait(ev.cond, ev.lock)
-    ev.signal = false
-    release(ev.lock)
-
-  proc setEvent*(ev: var Event) =
-    acquire(ev.lock)
-    ev.signal = true
-    signal(ev.cond)
-    release(ev.lock)
+proc setEvent*(ev: var Event) =
+  acquire(ev.lock)
+  ev.signal = true
+  signal(ev.cond)
+  release(ev.lock)
