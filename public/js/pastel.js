69f927e566a80f96faf12c627553f23a4173301c
zenywallet <zenywallet@bitzeny.jp>
2019-09-22 17:08:18 +0900

    client db xpub

--- public/js/pastel.js
+++ public/js/pastel.js
@@ -210,6 +210,63 @@ var Stream = (function() {
   return Stream;
 }());
 
+var Stor = (function() {
+  var db;
+  function Stor() {
+    if(this instanceof Stor) {
+      db = localStorage;
+    } else {
+      return new Stor();
+    }
+  }
+
+  Stor.prototype.add_xpub = function(xpub) {
+    var xpubs = db.getItem('xpubs');
+    if(xpubs) {
+      xpubs = JSON.parse(xpubs);
+    } else {
+      xpubs = [];
+    }
+    if(!xpubs.includes(xpub)) {
+      xpubs.push(xpub);
+      db.setItem('xpubs', JSON.stringify(xpubs));
+    }
+  }
+
+  Stor.prototype.get_xpubs = function() {
+    var xpubs = db.getItem('xpubs');
+    if(xpubs) {
+      xpubs = JSON.parse(xpubs);
+    } else {
+      xpubs = [];
+    }
+    return xpubs;
+  }
+
+  Stor.prototype.del_xpub = function(xpub) {
+    var xpubs = db.getItem('xpubs');
+    if(xpubs) {
+      xpubs = JSON.parse(xpubs);
+      var idx = xpubs.indexOf(xpub);
+      if(idx >= 0) {
+        xpubs.splice(idx, 1);
+        db.setItem('xpubs', JSON.stringify(xpubs));
+      }
+    } else {
+      xpubs = [];
+    }
+    if(xpubs.length == 0) {
+      db.removeItem('xpubs');
+    }
+  }
+
+  Stor.prototype.del_xpubs = function() {
+    db.removeItem('xpubs');
+  }
+
+  return Stor;
+}());
+
 pastel.ready = function() {
   console.log('pastel ready');
   console.log(pastel);
