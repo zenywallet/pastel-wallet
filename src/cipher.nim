8f7bafff33937038cba8a26186d8665461ea782d
zenywallet <zenywallet@bitzeny.jp>
2019-09-30 08:14:43 +0900

    cipher test

--- src/cipher.nim
+++ src/cipher.nim
@@ -13,3 +13,20 @@ proc cipher_enc*(in_blk, out_blk: ptr UncheckedArray[byte]) {.exportc: "cipher_e
 
 proc cipher_dec*(in_blk, out_blk: ptr UncheckedArray[byte]) {.exportc: "cipher_dec".} =
   ctr.decrypt(in_blk, out_blk)
+
+when isMainModule:
+  var k: array[32, byte]
+  var rs: array[32, byte]
+  var rc: array[32, byte]
+  var a, b, c: array[16, byte]
+  for i in 0..<32:
+    k[i] = cast[byte](i)
+    rs[i] = cast[byte](0)
+    rc[i] = cast[byte](0)
+  for i in 0..<16:
+    a[i] = cast[byte](0)
+  cipher_init(k, rc, rs)
+  cipher_enc(cast[ptr UncheckedArray[byte]](addr a[0]), cast[ptr UncheckedArray[byte]](addr b[0]))
+  echo b
+  cipher_dec(cast[ptr UncheckedArray[byte]](addr b[0]), cast[ptr UncheckedArray[byte]](addr c[0]))
+  echo c
