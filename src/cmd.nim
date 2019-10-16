1d4f24dfe5c584fe7c3d1d33086571746f1a9465
zenywallet <zenywallet@bitzeny.jp>
2019-10-17 00:53:48 +0900

    reset color attribute

--- src/cmd.nim
+++ src/cmd.nim
@@ -5,13 +5,13 @@ const blockstor_apikey = "sample-969a6d71-a259-447c-a486-90bac964992b"
 
 proc cmd_main() {.thread.} =
   while true:
-    stdout.write "\e[1;36m> \e[00m"
+    stdout.write "\e[1;36m> \e[0m"
     var cmd = stdin.readLine()
     if cmd.len > 0:
-      echo  "\e[1;36mcmd: ", cmd, "\e[00m"
+      echo  "\e[1;36mcmd: ", cmd, "\e[0m"
       if cmd == "wallets":
         for d in db.getWallets(""):
-          echo "\e[1;34m", d, "\e[00m"
+          echo "\e[1;34m", d, "\e[0m"
       elif cmd == "delwallets":
         db.delWallets();
         let smarker = blockstor.setMarker(blockstor_apikey, 0);
