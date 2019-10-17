10b014dadbdca764e8d74f308540d8bbb9395e59
zenywallet <zenywallet@bitzeny.jp>
2019-10-17 09:02:09 +0900

    use parseopt for terminal command

--- src/cmd.nim
+++ src/cmd.nim
@@ -1,20 +1,29 @@
 # Copyright (c) 2019 zenywallet
 
-import terminal, db, blockstor
+import terminal, parseopt, db, blockstor
 const blockstor_apikey = "sample-969a6d71-a259-447c-a486-90bac964992b"
 
 proc cmd_main() {.thread.} =
   while true:
     stdout.styledWrite(styleBright, fgCyan, "> ")
     var cmd = stdin.readLine()
-    if cmd.len > 0:
-      stdout.styledWriteLine(styleBright, fgCyan, "cmd: ", cmd)
-      if cmd == "wallets":
-        for d in db.getWallets(""):
-          stdout.styledWriteLine(styleBright, fgBlue, $d)
-      elif cmd == "delwallets":
-        db.delWallets();
-        let smarker = blockstor.setMarker(blockstor_apikey, 0);
+    stdout.styledWriteLine(styleBright, fgCyan, "cmd: ", cmd)
+    var p = initOptParser(cmd)
+    while true:
+      p.next()
+      case p.kind
+      of cmdEnd: break
+      of cmdArgument:
+        if p.key == "wallets":
+          p.next()
+          for d in db.getWallets(if p.kind == cmdEnd: "" else: p.key):
+            stdout.styledWriteLine(styleBright, fgBlue, $d)
+          break
+        elif p.key == "delwallets":
+          db.delWallets()
+          discard blockstor.setMarker(blockstor_apikey, 0)
+          break
+      else: discard
 
 var cmd_thread: Thread[void]
 
