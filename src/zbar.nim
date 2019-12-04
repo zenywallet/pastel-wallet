1d4f3edebe0de8bb3cf3dd50334fde9aa0f50fd7
zenywallet <zenywallet@bitzeny.jp>
2019-12-04 10:30:04 +0900

    test zbar wasm

--- src/zbar.nim
+++ src/zbar.nim
@@ -11,13 +11,48 @@ const zbarPath = splitPath(currentSourcePath()).head & "/../deps/zbar"
 {.passL: zbarPath & "/zbar/.libs/libzbar.a".}
 
 {.emit: """
+#include <stdint.h>
 #include "../deps/zbar/include/zbar.h"
+#include <emscripten.h>
 
-void zbar_test() {
-  zbar_image_scanner_create();
+int zbar_scan(uint8_t *raw, int width, int height)
+{
+  zbar_image_scanner_t *scanner = zbar_image_scanner_create();
+  zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_X_DENSITY, 1);
+  zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_Y_DENSITY, 1);
+
+  zbar_image_t *image = zbar_image_create();
+  zbar_image_set_format(image, zbar_fourcc('Y', '8', '0', '0'));
+  zbar_image_set_size(image, width, height);
+  zbar_image_set_data(image, raw, width * height, zbar_image_free_data);
+
+  int n = zbar_scan_image(scanner, image);
+
+  const zbar_symbol_t *symbol = zbar_image_first_symbol(image);
+  for(; symbol; symbol = zbar_symbol_next(symbol))
+  {
+    zbar_symbol_type_t typ = zbar_symbol_get_type(symbol);
+    const char *data = zbar_symbol_get_data(symbol);
+
+    unsigned poly_size = zbar_symbol_get_loc_size(symbol);
+
+    int poly[poly_size * 2];
+    unsigned u = 0;
+    for(unsigned p = 0; p < poly_size; p++)
+    {
+      poly[u] = zbar_symbol_get_loc_x(symbol, p);
+      poly[u + 1] = zbar_symbol_get_loc_y(symbol, p);
+      u += 2;
+    }
+
+    EM_ASM({
+      zbar.stream($0, $1, $2, $3);
+    }, zbar_get_symbol_name(typ), data, poly, poly_size);
+  }
+
+  zbar_image_destroy(image);
+  zbar_image_scanner_destroy(scanner);
+
+  return (0);
 }
 """.}
-
-proc zbar_test*() {.importc.}
-
-zbar_test()
