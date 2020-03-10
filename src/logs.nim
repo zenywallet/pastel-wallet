82a70969082df4bd0a37b0df7eadccfc6506989f
zenywallet <zenywallet@bitzeny.jp>
2020-03-10 11:15:36 +0900

    debug message type

--- src/logs.nim
+++ src/logs.nim
@@ -1,15 +1,37 @@
 # Copyright (c) 2019 zenywallet
 
-var debugMode = false
+import bitops
+
+type Debug* {.pure.} = enum
+  All
+  Common
+  Connection
+
+var msgflag: uint32
+
+proc enable*(msgtype: Debug) =
+  setBit(msgflag, msgtype.ord)
+
+proc disable*(msgtype: Debug) =
+  clearBit(msgflag, msgtype.ord)
+
+proc check*(msgtype: Debug): bool =
+  testBit(msgflag, msgtype.ord)
 
 proc debugEnable*() =
-  debugMode = true
+  Debug.All.enable()
 
 proc debugDisable*() =
-  debugMode = false
+  Debug.All.disable()
 
 template debug*(x: varargs[string, `$`]) =
-  if debugMode:
+  if testBit(msgflag, Debug.Common.ord) or testBit(msgflag, Debug.All.ord):
+    for s in x:
+      stdout.write s
+    stdout.writeLine ""
+
+template write*(msgtype: Debug, x: varargs[string, `$`]) =
+  if testBit(msgflag, msgtype.ord) or testBit(msgflag, Debug.All.ord):
     for s in x:
       stdout.write s
     stdout.writeLine ""
@@ -17,3 +39,11 @@ template debug*(x: varargs[string, `$`]) =
 when isMainModule:
   var a: int = 12345
   debug "test", $a
+
+  Debug.Connection.write "hello?"
+  Debug.Connection.enable()
+  Debug.Connection.write "hello!"
+  Debug.Connection.disable()
+  Debug.Connection.write "hello?"
+  Debug.All.enable()
+  Debug.Connection.write "hello!"
