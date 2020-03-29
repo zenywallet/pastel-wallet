9659c644f7c35d388e4905008a0ee39ee2187d2d
zenywallet <zenywallet@bitzeny.jp>
2020-03-30 00:57:29 +0900

    change text input mnemonic

--- public/js/main.nim
+++ public/js/main.nim
@@ -1493,7 +1493,7 @@ proc appMain(data: RouterData): VNode =
           tdiv(class="intro-head"):
             tdiv(class="caption"): text "Pastel Wallet"
             tdiv(class="ui container method-selector"):
-              tdiv(class="title"): text "Scan your seed cards or mnemonic to start."
+              tdiv(class="title"): text "Scan your seed cards or input your mnemonic to start."
               tdiv(class="ui buttons"):
                 button(id="seedselector", class="ui olive button", onclick=importSelector(ImportType.SeedCard)):
                   italic(class="qrcode icon")
