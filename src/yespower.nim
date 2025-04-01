36cf099e252384510c3269d69f4d5ad0c610acc6
zenywallet <zenywallet@bitzeny.jp>
2025-04-02 01:03:14 +0900

    Revert "change yespower hash"
    This reverts commit f3775f6b849b4205061801cd5f7b40b3b2216796.

--- src/yespower.nim
+++ src/yespower.nim
@@ -3,55 +3,12 @@
 
 {.compile: "../deps/yespower-1.0.1/sha256.c".}
 {.compile: "../deps/yespower-1.0.1/yespower-opt.c".}
-{.emit: """
-#include <string.h>
-#include "../deps/yespower-1.0.1/yespower.h"
+{.compile: "yespower_hash.c".}
 
-static yespower_params_t params = {
-  .version = YESPOWER_0_5,
-  .N = 2048,
-  .r = 8,
-  .pers = (const uint8_t *)"Client Key",
-  .perslen = strlen("Client Key")
-};
-
-static yespower_params_t params_n2r8 = {
-  .version = YESPOWER_1_0,
-  .N = 2048,
-  .r = 8,
-  .pers = (const uint8_t *)NULL,
-  .perslen = 0
-};
-
-static yespower_params_t params_n4r16 = {
-  .version = YESPOWER_1_0,
-  .N = 4096,
-  .r = 16,
-  .pers = (const uint8_t *)NULL,
-  .perslen = 0
-};
-
-static yespower_params_t params_n4r32 = {
-  .version = YESPOWER_1_0,
-  .N = 4096,
-  .r = 32,
-  .pers = (const uint8_t *)NULL,
-  .perslen = 0
-};
-""".}
-
-proc yespower_hash*(input: ptr UncheckedArray[byte], input_size: int, output: ptr UncheckedArray[byte]): int {.exportc.} =
-  {.emit: "return yespower_tls((uint8_t *)`input`, `input_size`, &params, `output`);".}
-
-proc yespower_n2r8*(input: ptr UncheckedArray[byte], input_size: int, output: ptr UncheckedArray[byte]): int {.exportc.} =
-  {.emit: "return yespower_tls((uint8_t *)`input`, `input_size`, &params_n2r8, `output`);".}
-
-proc yespower_n4r16*(input: ptr UncheckedArray[byte], input_size: int, output: ptr UncheckedArray[byte]): int {.exportc.} =
-  {.emit: "return yespower_tls((uint8_t *)`input`, `input_size`, &params_n4r16, `output`);".}
-
-proc yespower_n4r32*(input: ptr UncheckedArray[byte], input_size: int, output: ptr UncheckedArray[byte]): int {.exportc.} =
-  {.emit: "return yespower_tls((uint8_t *)`input`, `input_size`, &params_n4r32, `output`);".}
-
+proc yespower_hash*(input: ptr UncheckedArray[byte], input_size: int, output: ptr UncheckedArray[byte]): int {.importc.}
+proc yespower_n2r8*(input: ptr UncheckedArray[byte], input_size: int, output: ptr UncheckedArray[byte]): int {.importc.}
+proc yespower_n4r16*(input: ptr UncheckedArray[byte], input_size: int, output: ptr UncheckedArray[byte]): int {.importc.}
+proc yespower_n4r32*(input: ptr UncheckedArray[byte], input_size: int, output: ptr UncheckedArray[byte]): int {.importc.}
 
 when isMainModule:
   var a: array[80, byte]
