ef86d1b4c7e0d1485fefec66e41d30d3df66d17d
zenywallet <zenywallet@bitzeny.jp>
2025-10-12 01:36:25 +0900

    remove serpent

--- src/serpent.nim
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2019 zenywallet
-# nim c -d:release -d:emscripten -o:serpent.js serpent.nim
-
-{.compile: "../deps/serpent/serpent.c".}
-
-proc cipher_name*(): cstringArray {.importc: "serpent_cipher_name".}
-proc set_key*(in_key: ptr uint32; key_len: uint32): ptr uint32 {.importc: "serpent_set_key".}
-proc encrypt*(in_blk: ptr array[4, uint32]; out_blk: ptr array[4, uint32]) {.importc: "serpent_encrypt".}
-proc decrypt*(in_blk: ptr array[4, uint32]; out_blk: ptr array[4, uint32]) {.importc: "serpent_decrypt".}
