2f110fcd277851e8fb5995c60671d5d8fbb7fcdc
zenywallet <zenywallet@bitzeny.jp>
2026-06-07 00:58:44 +0900

    change config

--- src/config.nim
+++ src/config.nim
@@ -3,8 +3,6 @@
 import zenyjs
 import zenyjs/core
 import zenyjs/address
-import std/os
-import std/macros
 
 #{.define: ROCKSDB_DEFAULT_COMPRESSION.}
 #{.define: PASTEL_BITZENY_JP.}
@@ -27,39 +25,43 @@ networks:
 
 var network* = BitZeny_mainnet
 
-const blockstor_apiurl* = "http://localhost:8000/api/"
-const blockstor_apikey* = "sample-969a6d71-a259-447c-a486-90bac964992b"
-const blockstor_wshost* = "localhost"
-const blockstor_wsport* = 8001
+when not defined(js) and not defined(emscripten):
+  import std/os
+  import std/macros
 
-const gaplimit*: uint32 = 20
+  const blockstor_apiurl* = "http://localhost:8000/api/"
+  const blockstor_apikey* = "sample-969a6d71-a259-447c-a486-90bac964992b"
+  const blockstor_wshost* = "localhost"
+  const blockstor_wsport* = 8001
 
-when defined(PASTEL_BITZENY_JP) or os.getEnv("PASTEL_BITZENY_JP") == "1":
-  const HttpsPort* = 443
-  const HttpsHost* = "pastel.bitzeny.jp"
-  const HttpPort* = 80
-  const HttpHost* = "pastel.bitzeny.jp"
-  const HttpRedirect* = "https://pastel.bitzeny.jp"
-  const WebSocketUrl* = "wss://pastel.bitzeny.jp/ws"
-  const AcmePath* = "www/pastel.bitzeny.jp/"
-else:
-  const HttpsPort* = 5002
-  const HttpsHost* = "localhost:5002"
-  const HttpPort* = 5000
-  const HttpHost* = "localhost:5000"
-  const HttpRedirect* = "https://localhost:5002"
-  const WebSocketUrl* = "wss://localhost:5002/ws"
-  const AcmePath* = "www/localhost/"
+  const gaplimit*: uint32 = 20
+
+  when defined(PASTEL_BITZENY_JP) or os.getEnv("PASTEL_BITZENY_JP") == "1":
+    const HttpsPort* = 443
+    const HttpsHost* = "pastel.bitzeny.jp"
+    const HttpPort* = 80
+    const HttpHost* = "pastel.bitzeny.jp"
+    const HttpRedirect* = "https://pastel.bitzeny.jp"
+    const WebSocketUrl* = "wss://pastel.bitzeny.jp/ws"
+    const AcmePath* = "www/pastel.bitzeny.jp/"
+  else:
+    const HttpsPort* = 5002
+    const HttpsHost* = "localhost:5002"
+    const HttpPort* = 5000
+    const HttpHost* = "localhost:5000"
+    const HttpRedirect* = "https://localhost:5002"
+    const WebSocketUrl* = "wss://localhost:5002/ws"
+    const AcmePath* = "www/localhost/"
 
 
-macro writeConfigJs() =
-  const srcDir = currentSourcePath().parentDir()
-  writeFile(srcDir / "../public/js/config.js", """
-var pastel = pastel || {};
-pastel.config = pastel.config || {
-  ws_url: '""" & WebSocketUrl & """',
-  ws_protocol: 'pastel-v0.1',
-  network: 'bitzeny'
-};
-""")
-writeConfigJs()
+  macro writeConfigJs() =
+    const srcDir = currentSourcePath().parentDir()
+    writeFile(srcDir / "../public/js/config.js", """
+  var pastel = pastel || {};
+  pastel.config = pastel.config || {
+    ws_url: '""" & WebSocketUrl & """',
+    ws_protocol: 'pastel-v0.1',
+    network: 'bitzeny'
+  };
+  """)
+  writeConfigJs()
